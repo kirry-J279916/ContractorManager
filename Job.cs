cf030f04275eb5a8bbdcba10f9d5d688befd7cb1
j279916 <j279916@tafe.wa.edu.au>
2025-10-29 11:41:21 +0800

    Add Job class with properties and constructor

---
+++ Job.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContractorManager
+{
+    internal class Job
+    {
+        // -- Properties
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Cost { get; set; }
+        public bool Completed { get; set; }
+        public Contractor ContractorAssigned { get; set; }
+
+
+        // -- Constructor
+        public Job(string title, DateTime date, decimal cost)
+        {
+            Title = title;
+            Date = date;
+            Cost = cost;
+            Completed = false;
+            ContractorAssigned = null;
+        }
+    }
+}
